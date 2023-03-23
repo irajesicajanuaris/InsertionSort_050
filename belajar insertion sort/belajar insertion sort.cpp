d5aa87c20159291846f8075145d8477f540b85ce
irajesicajanuaris <jjiraaa1@gmail.com>
2023-03-23 10:39:55 +0700

    membuat Array dan Variable

--- belajar insertion sort/belajar insertion sort.cpp
+++ belajar insertion sort/belajar insertion sort.cpp
@@ -1,29 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
-int n;
+int arr[20];		// Membuat Array dengan panjanng data 20
+int n;				// Mmebuat Variable inputan n
 
-void input() {
-	while (true)
-	{
-		cout << "Masukkan Jumlah Data pada Array : ";
-		cin >> n;
-
-		if (n <= 20) {
-			break;
-		}
-		else
-		{
-			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
-		}
-	}
-	cout << endl;
-	cout << "======================" << endl;
-	cout << "Masukkan Element Array" << endl;
-	cout << "======================" << endl;
-
-	for (int i = 0; i < n; i++) {
-
-	}
-}
